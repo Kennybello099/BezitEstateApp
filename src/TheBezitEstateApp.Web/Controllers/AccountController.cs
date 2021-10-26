389150d97e84cb5f32f92d5be1d0c27a7cf5aebd
kennybello099 <kennybello099@gmail.com>
2021-10-26 19:17:10 +0100

    added Login UI

---
+++ src/TheBezitEstateApp.Web/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheBezitEstateApp.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
