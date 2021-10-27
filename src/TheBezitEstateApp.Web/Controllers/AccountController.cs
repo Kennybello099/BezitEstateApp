e12f08bb924581dfa774579ccf229ad1f14970bd
kennybello099 <kennybello099@gmail.com>
2021-10-27 10:52:59 +0100

    added models for login/register

--- src/TheBezitEstateApp.Web/Controllers/AccountController.cs
+++ src/TheBezitEstateApp.Web/Controllers/AccountController.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TheBezitEstateApp.Web.Models;
 
 namespace TheBezitEstateApp.Web.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if(!ModelState.IsValid) return View();
+
+            throw new NotImplementedException();
+        }
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if(!ModelState.IsValid) return View();
+
+            throw new NotImplementedException();
         }
     }
 }
